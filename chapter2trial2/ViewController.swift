0a62e3429abbaa5651fce170d356af7b390863ad
Wei Deng <weideng@Wei-Dengs-MacBook-Pro.local>
2017-02-19 18:58:03 +0800

    Initial Commit

---
+++ chapter2trial2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chapter2trial2
+//
+//  Created by Wei Deng on 19/2/17.
+//  Copyright © 2017 Wei Deng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
